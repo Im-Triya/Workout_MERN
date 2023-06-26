49c2e4e615c0838fee3146d262a6e679db7acdf8
unknown <reicha.triya.roy@gmail.com>
2023-06-26 18:16:56 +0530

    test

---
+++ backend/routes/workouts.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController.js");
+const router = express.Router();
+
+// Get all workouts
+router.get("/", getWorkouts);
+
+// Get a single workout
+router.get("/:id", getWorkout);
+
+// POST a new workout
+router.post("/", createWorkout);
+
+// DELETE a new workout
+router.delete("/:id", deleteWorkout);
+
+// UPDATE a new workout
+router.patch("/:id", updateWorkout);
+
+module.exports = router;
